dd853365382701f44a26d381648bcda6aaccf321
DemevengDerrick <demeveng@gmail.com>
2023-05-19 12:02:35 +0100

    Updated views

--- django_virtual_env/barcode_scanner/odk_barcode_app/urls.py
+++ django_virtual_env/barcode_scanner/odk_barcode_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('odk_barcode_app/login', views.ona_connector, name='login'),
+    path('', views.main, name='main'),
+    path('odk_barcode_app', views.ona_connector, name='ona_connector'),
     path('odk_barcode_app/scanner', views.scanner, name='scanner'),
     path('odk_barcode_app/record', views.display_data, name='record'),
 ]
