dc03ffe5c91605f63a6108ccbcd6b41e4af211a4
DemevengDerrick <demeveng@gmail.com>
2023-05-21 15:53:31 +0100

    Added login logic

--- django_virtual_env/barcode_scanner/odk_barcode_app/views.py
+++ django_virtual_env/barcode_scanner/odk_barcode_app/views.py
@@ -9,9 +9,12 @@ def ona_connector(request):
     return HttpResponse(template.render())
 
 def scanner(request):
-    template = loader.get_template('scanner.html')
+    template_login = loader.get_template('login.html')
+    template_main = loader.get_template('main.html')
     # Open the camera
     cap = cv2.VideoCapture(0)
+
+    global barcode_data
 
     barcode_data = "No barcode found."
 
@@ -28,10 +31,9 @@ def scanner(request):
         # Loop over detected barcodes
         for barcode in barcodes:
             # Extract barcode data
-            # Draw a bounding box around the barcode
-            
             barcode_data = barcode.data.decode("utf-8")
 
+            # Draw a bounding box around the barcode
             (x, y, w, h) = barcode.rect
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
@@ -53,19 +55,33 @@ def scanner(request):
 
     context = {'barcode': barcode_data}
 
-    # Display the barcode data in the browser using the template
-    return HttpResponse(template.render(context, request))
+    if barcode_data == "No barcode found.":
+        # Display the barcode data in the browser using the template
+        return HttpResponse(template_main.render(context, request))
+    else:
+        return HttpResponse(template_login.render(context, request))
 
 def display_data(request):
-    template = loader.get_template('record.html')
-    rq = requests.get("https://esurv.afro.who.int/api/v1/data/8604.json", auth=("gis_blueline", "G1sb!ue")).json()
 
-    for record in rq:
-        if record['epid_num'] == "ENV-ETH-SOM-FAF-SAG-17-001":
+    if request.method == 'POST':
+        name = request.POST['uname']
+        psw = request.POST['psw']
+
+    template = loader.get_template('record.html')
+    #rq = requests.get("https://esurv.afro.who.int/api/v1/data/8604.json", auth=("gis_blueline", "G1sb!ue")).json()
+    rq = requests.get("https://esurv.afro.who.int/api/v1/data/8604.json", auth=(name, psw)).json()
+
+    record = ""
+    for rec in rq:
+        if rec['epid_num'] == "ENV-ETH-SOM-FAF-SAG-17-001":
+            record = rec
             break
 
-    context = {'rq': record}
-    
+    if len(record) == 0:
+        context = {'rq': "No matching record found"}
+    else:
+        context = {'rq': record}
+
     return HttpResponse(template.render(context, request))
 
 def main(request):
